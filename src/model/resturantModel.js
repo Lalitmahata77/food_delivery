e7805ad2936946a95efc92f40e746304974bc9a1
Lalit-mahata <lm3654725@gmail.com>
2024-07-22 21:31:14 +0545

    resturantModel

--- src/model/resturantModel.js
+++ src/model/resturantModel.js
@@ -2,8 +2,38 @@ import mongoose from "mongoose"
 const resturantModel = new mongoose.Schema({
 owner : {
     type : mongoose.Schema.Types.ObjectId,
-
-}
+    ref : "User"
+},
+name : {
+    type : String,
+    required : true
+},
+description : {
+    type : String,
+    required : true
+},
+cuisineType : String,
+address : {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : "Address"
+},
+contactInformation : {},
+openingHourse : Boolean,
+orders : [{
+    type : mongoose.Schema.Types.ObjectId,
+    ref : "Order"
+}],
+rating : Number,
+images : [String],
+registratonDate : {
+    type : Date,
+    default : Date.now()
+},
+open : Boolean,
+foods : [{
+    type : mongoose.Schema.Types.ObjectId,
+    ref : "Food"
+}]
 },{timestamps : true})
 
 const Resturant = mongoose.model("Resturant", resturantModel)
